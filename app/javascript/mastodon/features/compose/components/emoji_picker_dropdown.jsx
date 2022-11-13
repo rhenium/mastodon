f5915d1e47db0f52793eab782ef45fe4acb5ded2
Kazuki Yamaguchi (don.nyanya.net) <k@rhe.jp>
2022-11-13 16:28:09 +0000

    [nnn] emoji_picker_dropdown: use U+1F463 as the picker icon
    It was recently changed in Mastodon main. It's so much better.... but I like
    this emoji more!
    
    WAS: [nnn] emoji_picker_dropdown: I don't like U+1F602

--- app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.jsx
+++ app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.jsx
@@ -388,8 +388,8 @@ class EmojiPickerDropdown extends PureComponent {
         <div ref={this.setTargetRef} className='emoji-button' title={title} aria-label={title} aria-expanded={active} role='button' onClick={this.onToggle} onKeyDown={this.onToggle} tabIndex={0}>
           {button || <img
             className={classNames('emojione', { 'pulse-loading': active && loading })}
-            alt='🙂'
-            src={`${assetHost}/emoji/1f642.svg`}
+            alt='👣'
+            src={`${assetHost}/emoji/1f463.svg`}
           />}
         </div>
 
