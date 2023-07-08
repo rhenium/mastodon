7acd142efd0a5b73027e039166793e9dfb5b6588
Kazuki Yamaguchi <k@rhe.jp>
2023-07-08 19:57:29 +0900

    [nnn] [outbox-fetch] fetch remote actor's outbox upon following
    Just one page for now.

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -185,6 +185,8 @@ namespace :api, format: false do
         post :unblock
         post :mute
         post :unmute
+
+        post :fetch_remote
       end
 
       resource :pin, only: :create, controller: 'accounts/pins'
