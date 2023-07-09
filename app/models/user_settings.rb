2d9bcfa124a2ad49fd4887bf244dfb7283dd757d
Kazuki Yamaguchi <k@rhe.jp>
2023-07-09 17:04:26 +0900

    [nnn] add an option to use bigger publish button
    This patch is based on work on GitHub akane-blue/mastodon:
    
     - akane-blue/mastodon@8a06ac2ca758 ("[maud] Add options for themes", 2022-04-01)
     - akane-blue/mastodon@e3675537c104 ("[maud] make features optional", 2022-04-02)

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -30,6 +30,7 @@ class UserSettings
     setting :expand_content_warnings, default: false
     setting :display_media, default: 'default', in: %w(default show_all hide_all)
     setting :auto_play, default: false
+    setting :mod_bigger_publish, default: false
   end
 
   namespace :notification_emails do
