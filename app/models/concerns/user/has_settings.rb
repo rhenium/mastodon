f6232e4d287143bb9cef2b7c3a464a79ef771c47
Kazuki Yamaguchi <k@rhe.jp>
2023-07-09 17:04:26 +0900

    [nnn] add an option to use bigger publish button
    This patch is based on work on GitHub akane-blue/mastodon:
    
     - akane-blue/mastodon@8a06ac2ca758 ("[maud] Add options for themes", 2022-04-01)
     - akane-blue/mastodon@e3675537c104 ("[maud] make features optional", 2022-04-02)

--- app/models/concerns/user/has_settings.rb
+++ app/models/concerns/user/has_settings.rb
@@ -134,4 +134,8 @@ module User::HasSettings
   def hide_all_media?
     settings['web.display_media'] == 'hide_all'
   end
+
+  def setting_bigger_publish
+    settings['web.mod_bigger_publish']
+  end
 end
