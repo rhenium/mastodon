7367e5c922b95a1e2d29de04a086a5b12f2c3a4c
Kazuki Yamaguchi <k@rhe.jp>
2023-07-09 17:04:26 +0900

    [nnn] add an option to use bigger publish button
    This patch is based on work on GitHub akane-blue/mastodon:
    
     - akane-blue/mastodon@8a06ac2ca758 ("[maud] Add options for themes", 2022-04-01)
     - akane-blue/mastodon@e3675537c104 ("[maud] make features optional", 2022-04-02)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -167,6 +167,7 @@ module ApplicationHelper
     output << "theme-#{current_theme.parameterize}"
     output << 'system-font' if current_account&.user&.setting_system_font_ui
     output << (current_account&.user&.setting_reduce_motion ? 'reduce-motion' : 'no-reduce-motion')
+    output << 'bigger-publish' if current_account&.user&.setting_bigger_publish
     output << 'rtl' if locale_direction == 'rtl'
     output.reject(&:blank?).join(' ')
   end
