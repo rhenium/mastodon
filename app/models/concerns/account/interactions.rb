20d9006622050e252d944201f01c715b3fbfb564
Kazuki Yamaguchi <k@rhe.jp>
2023-07-11 00:27:42 +0900

    [nnn] make remote media removals more conservative
    Preserve media attachments if:
    
     - the poster is followed by someone on this instance
     - the status has non-zero favs count, which means it was favorited by
       a local user
     - the status was reblogged by someone on this instance
     - the status was bookmarked by someone on this instance
    
    I will revisit this if the disk space is still a problem.

--- app/models/concerns/account/interactions.rb
+++ app/models/concerns/account/interactions.rb
@@ -74,6 +74,7 @@ module Account::Interactions
   included do
     # Follow relations
     has_many :follow_requests, dependent: :destroy
+    has_many :follow_requests_received, class_name: 'FollowRequest', foreign_key: 'target_account_id'
 
     with_options class_name: 'Follow', dependent: :destroy do
       has_many :active_relationships,  foreign_key: 'account_id', inverse_of: :account
