70826298ad8504f9bc58bbf85ea8d52a479233cd
Kazuki Yamaguchi <k@rhe.jp>
2023-07-13 14:58:18 +0900

    [nnn] [webui-translation] move translation link to action bar menu

--- app/javascript/mastodon/components/status_content.jsx
+++ app/javascript/mastodon/components/status_content.jsx
@@ -51,9 +51,7 @@ class TranslateButton extends PureComponent {
     }
 
     return (
-      <button className='status__content__translate-button' onClick={onClick}>
-        <FormattedMessage id='status.translate' defaultMessage='Translate' />
-      </button>
+      <></>
     );
   }
 
--- app/javascript/mastodon/features/status/components/action_bar.jsx
+++ app/javascript/mastodon/features/status/components/action_bar.jsx
@@ -59,10 +59,12 @@ const messages = defineMessages({
   unmute: { id: 'account.unmute', defaultMessage: 'Unmute @{name}' },
   unblock: { id: 'account.unblock', defaultMessage: 'Unblock @{name}' },
   openOriginalPage: { id: 'account.open_original_page', defaultMessage: 'Open original page' },
+  translate: { id: 'status.translate', defaultMessage: 'Translate' },
 });
 
 const mapStateToProps = (state, { status }) => ({
   relationship: state.getIn(['relationships', status.getIn(['account', 'id'])]),
+  languages: state.getIn(['server', 'translationLanguages', 'items']),
 });
 
 class ActionBar extends PureComponent {
@@ -92,6 +94,8 @@ class ActionBar extends PureComponent {
     onReport: PropTypes.func,
     onPin: PropTypes.func,
     onEmbed: PropTypes.func,
+    onTranslate: PropTypes.func,
+    languages: ImmutablePropTypes.map,
     intl: PropTypes.object.isRequired,
     ...WithRouterPropTypes,
   };
@@ -195,6 +199,10 @@ class ActionBar extends PureComponent {
     navigator.clipboard.writeText(url);
   };
 
+  handleTranslate = () => {
+    this.props.onTranslate(this.props.status);
+  };
+
   render () {
     const { status, relationship, intl } = this.props;
     const { signedIn, permissions } = this.context.identity;
@@ -223,6 +231,12 @@ class ActionBar extends PureComponent {
     }
 
     if (signedIn) {
+      // 2023-07-13: Conditions aken from app/javascript/mastodon/components/status_content.jsx
+      const contentLocale = intl.locale.replace(/[_-].*/, '');
+      const targetLanguages = this.props.languages?.get(status.get('language') || 'und');
+      if (status.get('search_index').trim().length > 0 && targetLanguages?.includes(contentLocale)) {
+        menu.push({ text: intl.formatMessage(messages.translate), action: this.handleTranslate });
+      }
       menu.push(null);
 
       if (writtenByMe) {
