ad6cd441fc6704f84ac173b802892253b5312ed2
Kazuki Yamaguchi <k@rhe.jp>
2023-07-13 14:58:18 +0900

    [nnn] [webui-translation] move translation link to action bar menu

--- app/javascript/mastodon/features/status/index.jsx
+++ app/javascript/mastodon/features/status/index.jsx
@@ -734,6 +734,7 @@ class Status extends ImmutablePureComponent {
                   onReport={this.handleReport}
                   onPin={this.handlePin}
                   onEmbed={this.handleEmbed}
+                  onTranslate={this.handleTranslate}
                 />
               </div>
             </HotKeys>
