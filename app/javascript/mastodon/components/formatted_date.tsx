82c638ff887a462a93d9ae2ca4c072747d96c687
Kazuki Yamaguchi <k@rhe.jp>
2025-01-11 12:54:47 +0900

    [better-timestamps-v5] Use better date format in FormattedDateWrapper
    Use intlFixes.formatDate() function instead of that from react-intl.

--- app/javascript/mastodon/components/formatted_date.tsx
+++ app/javascript/mastodon/components/formatted_date.tsx
@@ -1,18 +1,22 @@
-import type { ComponentProps } from 'react';
+import type { DateTimeFormatOptions } from '@/mastodon/utils/intl_fixes';
+import { intlFixes } from '@/mastodon/utils/intl_fixes';
 
-import { FormattedDate } from 'react-intl';
+type FormattedDateProps = DateTimeFormatOptions & {
+  value: Date | string | undefined;
+  className?: string;
+};
+export const FormattedDateWrapper = (props: FormattedDateProps) => {
+  // TODO: What is this? react-intl's FormattedDate appears to accept undefined
+  if (!props.value) return <time className={props.className} />;
 
-export const FormattedDateWrapper = (
-  props: ComponentProps<typeof FormattedDate> & { className?: string },
-) => (
-  <FormattedDate {...props}>
-    {(date) => (
-      <time dateTime={tryIsoString(props.value)} className={props.className}>
-        {date}
-      </time>
-    )}
-  </FormattedDate>
-);
+  const date = new Date(props.value);
+  const display = intlFixes.formatDate(date, props);
+  return (
+    <time dateTime={tryIsoString(props.value)} className={props.className}>
+      {display}
+    </time>
+  );
+};
 
 const tryIsoString = (date?: string | number | Date): string => {
   if (!date) {
